262b1de15253505c6cd9024f417ce96846e73ba1
SamTheisens <sam.theisens@gmail.com>
2021-11-14 11:23:08 +0800

    Correct power units for meter

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -18,20 +18,20 @@ export interface InputFormProps {
 
 export const InputForm: React.FunctionComponent<InputFormProps> = (props) => {
   const powerOptions: PowerOption[] = [
-    { name: '450 KVA', value: 450 },
-    { name: '900 KVA', value: 900 },
-    { name: '1300 KVA', value: 1300 },
-    { name: '2200 KVA', value: 2200 },
-    { name: '3500 KVA', value: 3500 },
-    { name: '3900 KVA', value: 3900 },
-    { name: '4400 KVA', value: 4400 },
-    { name: '5500 KVA', value: 5500 },
-    { name: '6600 KVA', value: 6600 },
-    { name: '7700 KVA', value: 7700 },
-    { name: '10600 KVA', value: 10600 },
-    { name: '11000 KVA', value: 11000 },
-    { name: '13200 KVA', value: 13200 },
-    { name: '16500 KVA', value: 16500 }
+    { name: '450 VA', value: 450 },
+    { name: '900 VA', value: 900 },
+    { name: '1.300 VA', value: 1300 },
+    { name: '2.200 VA', value: 2200 },
+    { name: '3.500 VA', value: 3500 },
+    { name: '3.900 VA', value: 3900 },
+    { name: '4.400 VA', value: 4400 },
+    { name: '5.500 VA', value: 5500 },
+    { name: '6.600 VA', value: 6600 },
+    { name: '7.700 VA', value: 7700 },
+    { name: '10.600 VA', value: 10600 },
+    { name: '11.000 VA', value: 11000 },
+    { name: '13.200 VA', value: 13200 },
+    { name: '16.500 VA', value: 16500 }
   ]
 
   const [form] = Form.useForm()
