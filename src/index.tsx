9cc789c00efd812bf763150bf1c73218b1a3a645
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2021-12-11 20:53:19 +0800

    Ensure ImportMeta env type is defined
    branch:

--- src/index.tsx
+++ src/index.tsx
@@ -5,4 +5,17 @@ import { App } from './App'
 import './i18n'
 import './index.css'
 
+declare global {
+  interface ImportMeta {
+    hot: {
+      accept: Function;
+      dispose: Function;
+    };
+    env: {
+      SNOWPACK_PUBLIC_SERVICE_WORKER: string;
+      SNOWPACK_PUBLIC_PACKAGE_VERSION: string;
+    };
+  }
+}
+
 ReactDOM.render(<Suspense fallback="loading..." ><App /></Suspense>, document.getElementById('root'))
