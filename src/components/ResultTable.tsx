ac98345b631543b690d8afbd9543d64f55244580
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2021-12-28 18:59:11 +0800

    Replace table with ant grid system
    branch:

--- src/components/ResultTable.tsx
+++ src/components/ResultTable.tsx
@@ -1,4 +1,4 @@
-import { Divider } from 'antd'
+import { Col, Divider, Row } from 'antd'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { ResultData } from '../services/CalculationService'
@@ -14,60 +14,53 @@ export const ResultTable: React.FunctionComponent<ResultTableProps> = (props) =>
   const { t, i18n } = useTranslation()
   const results = props.results
   const panels = Array.from(Array(results?.numberOfPanels).keys()).map(x => x + 1)
-  if (!results) { return <div>Invalid Data</div> }
+  if (!results) {
+    return <div>Invalid Data</div>
+  }
   return (
     <div className="ant-table">
-      <table className="results">
-        <tbody>
-          <tr>
-            <td colSpan={2}>{t('resultTable.recommendedPanels')}:</td>
-          </tr>
-          <tr>
-            <td colSpan={2} className="panelPane">
-              <div className="panelContainer">
-                {panels.map(renderPanel)}
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>{t('resultTable.installedCapacity')}</td>
-            <td>{formatDigits(results.numberOfPanels * CALCULATOR_VALUES.kiloWattPeakPerPanel, 2, i18n.language)} kWp</td>
-          </tr>
-          <tr>
-            <td>{t('resultTable.areaRequired')}</td>
-            <td>{formatDigits(results.numberOfPanels * CALCULATOR_VALUES.areaPerPanel, 0, i18n.language)} ㎡</td>
-          </tr>
-          <tr>
-            <td>{t('resultTable.monthlyConsumption')}</td>
-            <td><span>{`${formatNumber(results.consumptionPerMonthInKwh, i18n.language)} kWh`}</span></td>
-          </tr>
-          <tr>
-            <td>{t('resultTable.monthlyProduction')}</td>
-            <td>{`${formatNumber(results.productionPerMonthInKwh, i18n.language)} kWh`}</td>
-          </tr>
-        </tbody>
-      </table>
-      <Divider />
-      <table className="results">
-        <tbody>
-          <tr>
-            <td>{t('resultTable.currentMonthlyCosts')}</td>
-            <td>{formatRupiah(results.currentMonthlyCosts)}</td>
-          </tr>
-          <tr>
-            <td>{t('resultTable.remainingMonthlyCosts')}</td>
-            <td>{formatRupiah(results.remainingMonthlyCosts)}</td>
-          </tr>
-          <tr>
-            <td>{t('resultTable.monthlyProfit')}</td>
-            <td>{formatRupiah(results.currentMonthlyCosts - results.remainingMonthlyCosts)}</td>
-          </tr>
-          <tr>
-            <td>{t('resultTable.yearlyProfit')}</td>
-            <td>{formatRupiah((results.currentMonthlyCosts - results.remainingMonthlyCosts) * 12)}</td>
-          </tr>
-        </tbody>
-      </table>
+      <Row gutter={12} className="results">
+        <Col span={24}>{t('resultTable.recommendedPanels')}</Col>
+      </Row>
+      <Row gutter={12} className="panelPane">
+        <Col span={24} className="panelContainer">{panels.map(renderPanel)}</Col>
+      </Row>
+      <Row gutter={12} justify="end">
+        <Col span={15}>{t('resultTable.installedCapacity')}</Col>
+        <Col
+          span={9}>{formatDigits(results.numberOfPanels * CALCULATOR_VALUES.kiloWattPeakPerPanel, 2, i18n.language)} kWp</Col>
+      </Row>
+      <Row gutter={12} justify="center">
+        <Col span={15}>{t('resultTable.areaRequired')}</Col>
+        <Col
+          span={9}>{formatDigits(results.numberOfPanels * CALCULATOR_VALUES.areaPerPanel, 0, i18n.language)} ㎡</Col>
+      </Row>
+      <Row gutter={12} justify="center">
+        <Col span={15}>{t('resultTable.monthlyConsumption')}</Col>
+        <Col
+          span={9}><span>{`${formatNumber(results.consumptionPerMonthInKwh, i18n.language)} kWh`}</span></Col>
+      </Row>
+      <Row gutter={12}>
+        <Col span={15}>{t('resultTable.monthlyProduction')}</Col>
+        <Col span={9}>{`${formatNumber(results.productionPerMonthInKwh, i18n.language)} kWh`}</Col>
+      </Row>
+      <Divider/>
+      <Row gutter={12}>
+        <Col span={15}>{t('resultTable.currentMonthlyCosts')}</Col>
+        <Col span={9}>{formatRupiah(results.currentMonthlyCosts)}</Col>
+      </Row>
+      <Row gutter={12}>
+        <Col span={15}>{t('resultTable.remainingMonthlyCosts')}</Col>
+        <Col  span={9}>{formatRupiah(results.remainingMonthlyCosts)}</Col>
+      </Row>
+      <Row gutter={12}>
+        <Col span={15}>{t('resultTable.monthlyProfit')}</Col>
+        <Col  span={9}>{formatRupiah(results.currentMonthlyCosts - results.remainingMonthlyCosts)}</Col>
+      </Row>
+      <Row gutter={12}>
+        <Col span={15}>{t('resultTable.yearlyProfit')}</Col>
+        <Col  span={9}>{formatRupiah((results.currentMonthlyCosts - results.remainingMonthlyCosts) * 12)}</Col>
+      </Row>
     </div>
   )
 }
