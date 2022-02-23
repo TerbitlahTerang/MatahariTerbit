c7756657c3dd19e4977995117d975917c5fe0e81
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-02-23 22:17:18 +0800

    Fix alignment for playback period
    branch:

--- src/components/ResultTable.tsx
+++ src/components/ResultTable.tsx
@@ -110,14 +110,14 @@ export const ResultTable: React.FunctionComponent<ResultTableProps> = (props) =>
         <Col span={9}>{formatRupiah(results.yearlyProfit)}</Col>
       </Row>
       <Row gutter={12}>
-        <Col span={15}>{t('resultTable.breakEven')}&nbsp;
+        <Col span={11}>{t('resultTable.breakEven')}&nbsp;
           <Popover
             content={<InfoPane documentation={Documentation.RoiExplanation}/>}
             trigger="click">
             <InfoCircleOutlined/>
           </Popover>
         </Col>
-        <Col span={9}>{t('resultTable.breakEvenExplanation', { breakEven })}</Col>
+        <Col span={13}>{t('resultTable.breakEvenExplanation', { breakEven })}</Col>
       </Row>
       <Divider/>
     </div>
