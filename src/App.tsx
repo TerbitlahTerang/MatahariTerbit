be973abe4c86be0a559a367e69a3087a9e8b60ab
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-02-27 23:02:36 +0800

    Responsive width of drawer
    branch:

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ export const App: React.FunctionComponent = () => {
           </Select>
         </div>
       </nav>
-      <Drawer title={documentationTitle} visible={documentation !== null} onClose={closeDocumentation} width="500px" >
+      <Drawer title={documentationTitle} visible={documentation !== null} onClose={closeDocumentation} width={window.innerWidth > 900 ? '40%' : '82%'} >
         <InfoPane documentation={documentation!}/>
       </Drawer>
       <div className="card">
