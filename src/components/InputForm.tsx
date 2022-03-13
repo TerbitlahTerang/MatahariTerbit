4fc3e9c3cd2a3dd308722c4e71bba852837fb9d2
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-03-13 16:19:21 +0800

    Reflect changes of calculator settings on URL
    branch:

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -7,6 +7,7 @@ import { MapPicker } from './MapPicker'
 import { CALCULATOR_SETTINGS, CalculatorSettings, OptimizationTarget, PowerOption, powerOptions } from '../constants'
 import { InfoCircleOutlined } from '@ant-design/icons'
 import { Documentation } from '../services/DocumentationService'
+import { NumberParam, useQueryParam, withDefault } from 'use-query-params'
 
 export interface InputData {
   monthlyCostEstimateInRupiah: number
@@ -33,8 +34,25 @@ export const InputForm: React.FunctionComponent<InputFormProps> = (props) => {
   }
 
   const init = props.initialValue
-  const plnSettings = init.calculatorSettings.plnSettings
-  const priceSettings = init.calculatorSettings.priceSettings
+  const calcSettings = init.calculatorSettings
+  const plnSettings = calcSettings.plnSettings
+  const priceSettings = calcSettings.priceSettings
+
+  const [lowTariff, setLowTariff] = useQueryParam('lowTariff', withDefault(NumberParam, plnSettings.lowTariff))
+  const [highTariff, setHighTariff] = useQueryParam('highTariff', withDefault(NumberParam, plnSettings.highTariff))
+  const [energyTax, setEnergyTax] = useQueryParam('energyTax', withDefault(NumberParam, plnSettings.energyTax))
+
+  const [lowTariffThreshold, setLowTariffThreshold] = useQueryParam('lowTariffThreshold', withDefault(NumberParam, plnSettings.lowTariffThreshold))
+  const [minimalMonthlyConsumptionHours, setMinimalMonthlyConsumptionHours] = useQueryParam('minimalMonthlyConsumptionHours', withDefault(NumberParam, plnSettings.minimalMonthlyConsumptionHours))
+  const [minimalMonthlyConsumptionPrice, setMinimalMonthlyConsumptionPrice] = useQueryParam('minimalMonthlyConsumptionPrice', withDefault(NumberParam, plnSettings.minimalMonthlyConsumptionPrice))
+
+  const [pricePerPanel, setPricePerPanel] = useQueryParam('pricePerPanel', withDefault(NumberParam, priceSettings.pricePerPanel))
+  const [electricityPriceInflationRate, setElectricityPriceInflationRate] = useQueryParam('electricityPriceInflationRate', withDefault(NumberParam, priceSettings.electricityPriceInflationRate))
+  // const [priceOfInverterFactor, setPriceOfInverterFactor] = useQueryParam('priceOfInverterFactor', withDefault(NumberParam, priceSettings.priceOfInverterFactor))
+  const [capacityLossRate, setCapacityLossRate] = useQueryParam('capacityLossRate', withDefault(NumberParam, priceSettings.capacityLossRate))
+  const [kiloWattPeakPerPanel, setKiloWattPeakPerPanel] = useQueryParam('kiloWattPeakPerPanel', withDefault(NumberParam, calcSettings.kiloWattPeakPerPanel))
+  const [areaPerPanel, setAreaPerPanel] = useQueryParam('areaPerPanel', withDefault(NumberParam, calcSettings.areaPerPanel))
+  const [lossFromInverter, setLossFromInverter] = useQueryParam('lossFromInverter', withDefault(NumberParam, calcSettings.lossFromInverter))
 
   return (
     <Form form={form} layout="vertical" name="calculator" onFieldsChange={() => {
@@ -44,26 +62,25 @@ export const InputForm: React.FunctionComponent<InputFormProps> = (props) => {
       const pvOut = location.info?.pvout
       const optimizationTarget = form.getFieldValue('optimizationTarget') ? OptimizationTarget.Money : OptimizationTarget.Green
 
-      const calculatorSettings: CalculatorSettings = props.expertMode ? Object.assign<CalculatorSettings, { } >(
-        CALCULATOR_SETTINGS, { plnSettings: {
-          lowTariff: form.getFieldValue('lowTariff'),
-          highTariff: form.getFieldValue('highTariff'),
-          lowTariffThreshold: form.getFieldValue('lowTariffThreshold'),
-          energyTax: form.getFieldValue('energyTax'),
-          minimalMonthlyConsumptionHours: form.getFieldValue('minimalMonthlyConsumptionHours'),
-          minimalMonthlyConsumptionPrice: form.getFieldValue('minimalMonthlyConsumptionPrice')
-        },
-        priceSettings: {
-          pricePerPanel: form.getFieldValue('pricePerPanel'),
-          electricityPriceInflationRate: form.getFieldValue('electricityPriceInflationRate'),
-          priceOfInverterFactor: 0.10,
-          capacityLossRate: form.getFieldValue('capacityLossRate')
-        },
-        kiloWattPeakPerPanel: form.getFieldValue('kiloWattPeakPerPanel'),
-        areaPerPanel: form.getFieldValue('areaPerPanel'),
-        lossFromInverter: form.getFieldValue('lossFromInverter')
-        }
-      ): CALCULATOR_SETTINGS
+      const calculatorSettings: CalculatorSettings = props.expertMode ?  { plnSettings: {
+        lowTariff,
+        highTariff,
+        lowTariffThreshold,
+        energyTax,
+        minimalMonthlyConsumptionHours,
+        minimalMonthlyConsumptionPrice
+      },
+      priceSettings: {
+        pricePerPanel,
+        electricityPriceInflationRate,
+        priceOfInverterFactor: 0.10,
+        capacityLossRate
+      },
+      kiloWattPeakPerPanel,
+      areaPerPanel,
+      lossFromInverter
+      } as CalculatorSettings
+        : CALCULATOR_SETTINGS
 
       props.onChange({ monthlyCostEstimateInRupiah: consumption, connectionPower, pvOut, optimizationTarget, calculatorSettings })
     }}>
@@ -117,117 +134,149 @@ export const InputForm: React.FunctionComponent<InputFormProps> = (props) => {
         <Row gutter={16}>
           <Col xs={24} sm={8}>
             <Form.Item name="lowTariff" label={t('inputForm.expertMode.lowTariff')}
-              initialValue={plnSettings.lowTariff}
+              initialValue={lowTariff}
             >
               <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"
+                defaultValue={lowTariff}
                 formatter={(value) => formatRupiah(value)}
-                parser={(displayValue) => Number(displayValue ? +displayValue.replace(/Rp\.\s?|(,*)/g, '') : 0)}/>
+                parser={(displayValue) => Number(displayValue ? +displayValue.replace(/Rp\.\s?|(,*)/g, '') : 0)}
+                onChange={setLowTariff}
+              />
             </Form.Item>
           </Col>
           <Col xs={24} sm={8}>
             <Form.Item name="highTariff" label={t('inputForm.expertMode.highTariff')}
-              initialValue={plnSettings.highTariff}
+              initialValue={highTariff}
             >
               <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"
+                defaultValue={highTariff}
                 formatter={(value) => formatRupiah(value)}
-                parser={(displayValue) => Number(displayValue ? +displayValue.replace(/Rp\.\s?|(,*)/g, '') : 0)}/>
+                parser={(displayValue) => Number(displayValue ? +displayValue.replace(/Rp\.\s?|(,*)/g, '') : 0)}
+                onChange={setHighTariff}
+              />
             </Form.Item>
           </Col>
           <Col xs={24} sm={8}>
             <Form.Item name="lowTariffThreshold" label={t('inputForm.expertMode.lowTariffThreshold')}
-              initialValue={plnSettings.lowTariffThreshold}
+              initialValue={lowTariffThreshold}
             >
-              <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"/>
+              <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"
+                defaultValue={lowTariffThreshold}
+                onChange={setLowTariffThreshold}
+              />
             </Form.Item>
           </Col>
         </Row><Row gutter={16}>
           <Col xs={24} sm={4}>
             <Form.Item name="energyTax" label={t('inputForm.expertMode.energyTax')}
-              initialValue={plnSettings.energyTax}
+              initialValue={energyTax}
             >
               <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"
+                defaultValue={energyTax}
                 formatter={(value) => formatPercentage(value, i18n.language)}
                 parser={(displayValue) => parsePercentage(displayValue)}
+                onChange={setEnergyTax}
                 step={0.01}/>
             </Form.Item>
           </Col>
           <Col xs={24} sm={10}>
             <Form.Item name="minimalMonthlyConsumptionHours"
               label={t('inputForm.expertMode.minimalMonthlyConsumptionHours')}
-              initialValue={plnSettings.minimalMonthlyConsumptionHours}
+              initialValue={minimalMonthlyConsumptionHours}
             >
-              <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"/>
+              <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"
+                defaultValue={minimalMonthlyConsumptionHours} onChange={setMinimalMonthlyConsumptionHours}
+              />
             </Form.Item>
           </Col>
           <Col xs={24} sm={10}>
             <Form.Item name="minimalMonthlyConsumptionPrice"
               label={t('inputForm.expertMode.minimalMonthlyConsumptionPrice')}
-              initialValue={plnSettings.minimalMonthlyConsumptionPrice}
+              initialValue={minimalMonthlyConsumptionPrice}
             >
-              <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"/>
+              <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"
+                defaultValue={minimalMonthlyConsumptionPrice} onChange={setMinimalMonthlyConsumptionPrice}
+              />
             </Form.Item>
           </Col>
         </Row><Divider orientation="left">PLTS System settings</Divider><Row gutter={16}>
           <Col xs={24} sm={6}>
             <Form.Item name="pricePerPanel" label={t('inputForm.expertMode.pricePerPanel')}
-              initialValue={priceSettings.pricePerPanel}
+              initialValue={pricePerPanel}
             >
               <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"
+                defaultValue={pricePerPanel}
                 formatter={(value) => formatRupiah(value)}
                 parser={(displayValue) => Number(displayValue ? +displayValue.replace(/Rp\.\s?|(,*)/g, '') : 0)}
-                step={100000}/>
+                step={100000}
+                onChange={setPricePerPanel}
+              />
             </Form.Item>
           </Col>
           <Col xs={24} sm={8}>
             <Form.Item name="electricityPriceInflationRate"
               label={t('inputForm.expertMode.electricityPriceInflationRate')}
-              initialValue={priceSettings.electricityPriceInflationRate}
+              initialValue={electricityPriceInflationRate}
             >
               <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"
+                defaultValue={electricityPriceInflationRate}
                 formatter={(value) => formatPercentage(value, i18n.language, 2)}
                 parser={(displayValue) => parsePercentage(displayValue)}
-                step={0.01}/>
+                step={0.01}
+                onChange={setElectricityPriceInflationRate}
+              />
             </Form.Item>
           </Col>
           <Col xs={24} sm={10}>
             <Form.Item name="capacityLossRate" label={t('inputForm.expertMode.capacityLossRate')}
-              initialValue={priceSettings.capacityLossRate}
+              initialValue={capacityLossRate}
             >
               <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"
+                defaultValue={capacityLossRate}
                 formatter={(value) => formatPercentage(value, i18n.language, 2)}
                 parser={(displayValue) => parsePercentage(displayValue)}
-                step={0.001}/>
+                step={0.001}
+                onChange={setCapacityLossRate}
+              />
             </Form.Item>
           </Col>
         </Row><Row gutter={16}>
           <Col xs={24} sm={6}>
             <Form.Item name="kiloWattPeakPerPanel" label={t('inputForm.expertMode.kiloWattPeakPerPanel')}
-              initialValue={init.calculatorSettings.kiloWattPeakPerPanel}
+              initialValue={kiloWattPeakPerPanel}
             >
               <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"
+                defaultValue={kiloWattPeakPerPanel}
                 formatter={(value) => formatDigits(value, 3, i18n.language)}
                 parser={(displayValue) => parseNumber(displayValue)}
+                onChange={setKiloWattPeakPerPanel}
                 step={0.01}/>
             </Form.Item>
           </Col>
           <Col xs={24} sm={6}>
             <Form.Item name="areaPerPanel" label={t('inputForm.expertMode.areaPerPanel')}
-              initialValue={init.calculatorSettings.areaPerPanel}
+              initialValue={areaPerPanel}
             >
               <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"
+                defaultValue={areaPerPanel}
                 formatter={(value) => formatDigits(value, 2, i18n.language)}
                 parser={(displayValue) => parseNumber(displayValue)}
-                step={0.1}/>
+                step={0.1}
+                onChange={setAreaPerPanel}
+              />
             </Form.Item>
           </Col>
           <Col xs={24} sm={6}>
             <Form.Item name="lossFromInverter" label={t('inputForm.expertMode.lossFromInverter')}
-              initialValue={init.calculatorSettings.lossFromInverter}
+              initialValue={lossFromInverter}
             >
               <InputNumber style={{ width: '100%', textAlign: 'right' }} autoComplete="off"
+                defaultValue={lossFromInverter}
                 formatter={(value) => formatDigits(value, 4, i18n.language)}
                 parser={(displayValue) => parseNumber(displayValue)}
-                step={0.1}/>
+                step={0.1}
+                onChange={setLossFromInverter}
+              />
             </Form.Item>
           </Col>
         </Row>
