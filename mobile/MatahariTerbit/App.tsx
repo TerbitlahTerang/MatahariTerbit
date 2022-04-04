bcecb915fab87ccab2e60a033bb710dd88af2c12
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-04-04 20:47:30 +0800

    Add app icon
    branch:

--- mobile/MatahariTerbit/App.tsx
+++ mobile/MatahariTerbit/App.tsx
@@ -17,7 +17,7 @@ export default function App() {
   const langOnly = deviceLanguage.split('_')[0]
   return (
     Platform.OS === 'web' ? <iframe src="https://matahariterbit.web.app/" height={896} width={414}/> :
-      <NativeBaseProvider><View style={{ flex: 1 }}><WebView originWhitelist={['*']}
+      <NativeBaseProvider><View style={{ flex: 1 }} backgroundColor='#1890ff'><WebView originWhitelist={['*']}
         source={{ uri: `http://192.168.1.4:8080/?lng=${langOnly}&priorityEnabled=0`, baseUrl: '' }}
         style={{ flex: 1, height: 2 }}
       />
