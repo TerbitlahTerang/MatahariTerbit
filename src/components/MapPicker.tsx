912066607b4f2db09e616ad806ce6bd5178f1b98
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-04-24 17:58:00 +0800

    fixup! Remove google maps dependency

--- src/components/MapPicker.tsx
+++ src/components/MapPicker.tsx
@@ -1,10 +1,9 @@
 import { DownOutlined, UpOutlined } from '@ant-design/icons'
 import { Button } from 'antd'
-import { Coords, Point } from 'google-map-react'
 import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { DEFAULT_ZOOM, INITIAL_INPUT_DATA } from '../constants'
 import i18n from '../i18n'
-import { MapState, mapStore } from '../util/mapStore'
+import { Coords, MapState, mapStore } from '../util/mapStore'
 import { formatNumber } from '../services/Formatters'
 import { MapMarker } from './MapMarker'
 import './MapPicker.css'
@@ -23,7 +22,6 @@ export const MapPicker: React.FunctionComponent<MapPickerProps> = (props) => {
   const [position, setPosition] = useState<Coords>(props.value!.location)
   const [center, setCenter] = useState<Coords>(props.value!.location)
   const [zoom, setZoom] = useState<number>(DEFAULT_ZOOM)
-  const [draggable, setDraggable] = useState<boolean>(true)
   const [mapState, setMapState] = useState<MapState>(props.value!)
   const [collapsed, setCollapsed] = useState<boolean>(false)
 
@@ -37,11 +35,6 @@ export const MapPicker: React.FunctionComponent<MapPickerProps> = (props) => {
   const updatePosition = async (location: Coords) => {
     mapStore.setLocation(location)
     setPosition(location)
-  }
-
-  const onMouseDrag = (childKey: string, childProps: unknown, location: Coords) => {
-    setDraggable(false)
-    updatePosition(location)
   }
 
   const setLocation = (coordinates: Coords) => {
@@ -61,9 +54,6 @@ export const MapPicker: React.FunctionComponent<MapPickerProps> = (props) => {
 
   function LocationMarker() {
     const map = useMapEvents({
-      loading() {
-        map.locate()
-      },
       click(e) {
         setLocation(e.latlng)
         map.flyTo(e.latlng, map.getZoom(), { animate: true })
@@ -100,7 +90,7 @@ export const MapPicker: React.FunctionComponent<MapPickerProps> = (props) => {
             onClick={() => { setCollapsed(!collapsed) }} />
         </div>
         <div className="map-picker-view">
-          <MapContainer center={[center.lat, center.lng]} zoom={25} scrollWheelZoom={false} id='map'
+          <MapContainer center={[center.lat, center.lng]} zoom={zoom} scrollWheelZoom={false} id='map'
           >
             <TileLayer
               url='https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}'
