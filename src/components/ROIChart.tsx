024c3a441762967365bc84c67fb635400130ae9e
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-06-04 19:44:38 +0800

    Remove unused imports
    branch:

--- src/components/ROIChart.tsx
+++ src/components/ROIChart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import { Bar } from 'react-chartjs-2'
 import { ChartData, ChartOptions, Plugin, TooltipItem } from 'chart.js'
 import { useTranslation } from 'react-i18next'
