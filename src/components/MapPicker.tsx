dd9aa60493dbcaca14c7d89767c0564e028e6c48
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-07-01 00:53:43 +0800

    Remove test link
    branch:

--- src/components/MapPicker.tsx
+++ src/components/MapPicker.tsx
@@ -135,8 +135,6 @@ export const MapPicker: React.FunctionComponent<MapPickerProps> = ({ value, onCh
 
   return (
     <div>
-      <a href='solarcalculatorindonesia:://link?hello=world'><h3>Link to App</h3></a>
-
       <div className={`map-picker ${collapsed ? 'collapsed' : 'expanded'}`} >
         <div className="ant-input map-picker-header">
           {editMode && !collapsed ?
